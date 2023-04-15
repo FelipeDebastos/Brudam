5ea80c63069af2ebfdaa2a9d54f74fafac127f75
felipe.bastos <felipe.bastos@meta.com.br>
2023-04-15 19:06:46 -0300

    configuração do banco de dados e criação das tabelas via migrations

--- config/database.php
+++ config/database.php
@@ -66,17 +66,17 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', env('DB_HOST', 'localhost')),
+            'port' => env('DB_PORT', env('POSTGRES_PORT', '5432')),
+            'database' => env('DB_DATABASE', env('POSTGRES_DB', 'forge')),
+            'username' => env('DB_USERNAME', env('POSTGRES_USER', 'forge')),
+            'password' => env('DB_PASSWORD', env('POSTGRES_PASSWORD', '')),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
-        ],
+            ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
