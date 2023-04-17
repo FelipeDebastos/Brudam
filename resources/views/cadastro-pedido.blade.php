8b54d95c7e8ee77707ab4cf027052a7a5087312a
felipe.bastos <felipe.bastos@meta.com.br>
2023-04-17 01:06:30 -0300

    Correção nas blades e estilos

--- resources/views/cadastro-pedido.blade.php
+++ resources/views/cadastro-pedido.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Brudam</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
@@ -12,74 +14,70 @@
 
 <body class="antialiased">
     @foreach (['success', 'error', 'warning', 'info'] as $type)
-        @if (session()->has($type))
-            <div id="alert-{{ $type }}" class="alert alert-{{ $type }} {{ $type == 'error' ? 'alert-danger' : '' }}">
-                {{ session()->get($type) }}
-            </div>
-        @endif
+    @if (session()->has($type))
+    <div id="alert-{{ $type }}" class="alert alert-{{ $type }} {{ $type == 'error' ? 'alert-danger' : '' }}">
+        {{ session()->get($type) }}
+    </div>
+    @endif
     @endforeach
 
     <div class="form-container">
-        <form class="form-cadastro" action="{{ route('processar_pedido') }}" method="POST">
+        <form class="form-register" action="{{ route('processar_pedido') }}" method="POST">
             @csrf
             <h2>Cadastro de Pedido</h2>
 
             <div class="form-group">
-                <label for="nome">Nome:</label>
+                <label for="nome" class="form-label">Nome:</label>
                 <input type="text" class="form-control" id="nome" name="nome" required>
             </div>
 
             <div class="form-group">
-                <label for="email">E-mail:</label>
+                <label for="email" class="form-label">E-mail:</label>
                 <input type="email" class="form-control" id="email" name="email" required>
             </div>
 
             <div class="form-group">
-                <label for="telefone">Telefone:</label>
+                <label for="telefone" class="form-label">Telefone:</label>
                 <input type="tel" class="form-control" id="telefone" name="telefone" required>
             </div>
 
             <div class="form-group">
-                <label for="data_pedido">Data do pedido:</label>
+                <label for="data_pedido" class="form-label">Data do pedido:</label>
                 <input type="date" class="form-control" id="data_pedido" name="data_pedido" value="{{ date('Y-m-d') }}" required>
             </div>
 
             <div class="form-group">
-                <label for="valor_pedido">Valor do pedido:</label>
+                <label for="valor_pedido" class="form-label">Valor do pedido:</label>
                 <input type="text" class="form-control" id="valor_pedido" name="valor_pedido">
             </div>
 
             <div class="form-buttons-container">
                 <div>
-                    <button type="submit" class="btn btn-primary" id="register-button">
-                        Cadastrar
-                    </button>
-                    <button type="reset" class="btn btn-secondary" id="clean-button">
-                        Limpar
-                    </button>
+                    <button type="submit" class="btn btn-primary" id="register-button">Cadastrar</button>
+                    <button type="reset" class="btn btn-secondary" id="clean-button">Limpar</button>
                 </div>
                 <a href="{{ route('historico-pedido') }}" class="btn btn-secondary">Histórico de pedidos</a>
             </div>
         </form>
     </div>
-
-    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.mask.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <script>
-        $(document).ready(function() {
-            $('#valor_pedido').mask('000.000.000,00', {
-                reverse: true
-            });
-        });
-
-        setTimeout(function() {
-            $('#alert-success, #alert-error, #alert-warning, #alert-info').fadeOut('slow', function() {
-                $(this).remove();
-            });
-        }, 3000);
-    </script>
-
 </body>
+
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#valor_pedido').mask('000.000.000,00', {
+            reverse: true
+        });
+    });
+
+    setTimeout(function() {
+        $('#alert-success, #alert-error, #alert-warning, #alert-info').fadeOut('slow', function() {
+            $(this).remove();
+        });
+    }, 3000);
+</script>
