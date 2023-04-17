99b5b8370c1b34e3c42275cb8cd9e762707d273c
felipe.bastos <felipe.bastos@meta.com.br>
2023-04-17 01:12:51 -0300

    Criada máscara no campo de telefone

--- resources/views/cadastro-pedido.blade.php
+++ resources/views/cadastro-pedido.blade.php
@@ -73,6 +73,8 @@
         $('#valor_pedido').mask('000.000.000,00', {
             reverse: true
         });
+
+        $('#telefone').mask('(00) 00000-0000');
     });
 
     setTimeout(function() {
