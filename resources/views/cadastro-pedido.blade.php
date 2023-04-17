0cc815007652e94da11a74597238a7352dcaf571
felipe.bastos <felipe.bastos@meta.com.br>
2023-04-17 00:33:33 -0300

    Melhoria de codigo na blade de cadastro

--- resources/views/cadastro-pedido.blade.php
+++ resources/views/cadastro-pedido.blade.php
@@ -1,44 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Brudam</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-
-    <script>
-        $(document).ready(function() {
-            $('#valor_pedido').mask('000.000.000,00', {
-                reverse: true
-            });
-        });
-
-        setTimeout(function() {
-            $('#alert-success, #alert-error, #alert-warning, #alert-info').fadeOut('slow', function() {
-                $(this).remove();
-            });
-        }, 3000);
-    </script>
 
 </head>
 
 <body class="antialiased">
     @foreach (['success', 'error', 'warning', 'info'] as $type)
-    @if (session()->has($type))
-    <div id="alert-{{ $type }}" class="alert alert-{{ $type }} {{ $type == 'error' ? 'alert-danger' : '' }}">
-        {{ session()->get($type) }}
-    </div>
-    @endif
+        @if (session()->has($type))
+            <div id="alert-{{ $type }}" class="alert alert-{{ $type }} {{ $type == 'error' ? 'alert-danger' : '' }}">
+                {{ session()->get($type) }}
+            </div>
+        @endif
     @endforeach
 
     <div class="form-container">
@@ -73,7 +51,7 @@
 
             <div class="form-buttons-container">
                 <div>
-                    <button type="submit" class="btn btn-primary" id="register-button" >
+                    <button type="submit" class="btn btn-primary" id="register-button">
                         Cadastrar
                     </button>
                     <button type="reset" class="btn btn-secondary" id="clean-button">
@@ -85,7 +63,23 @@
         </form>
     </div>
 
-    </div>
-</body>
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.mask.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
+    <script>
+        $(document).ready(function() {
+            $('#valor_pedido').mask('000.000.000,00', {
+                reverse: true
+            });
+        });
+
+        setTimeout(function() {
+            $('#alert-success, #alert-error, #alert-warning, #alert-info').fadeOut('slow', function() {
+                $(this).remove();
+            });
+        }, 3000);
+    </script>
+
+</body>
 </html>
